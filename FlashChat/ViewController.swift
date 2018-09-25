ff321c93f2367e0e00e91a7e01e90803e629834b
Kartheek <kartheek.sprt@gmail.com>
2018-09-25 23:34:57 +0530

    initial commit

---
+++ FlashChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlashChat
+//
+//  Created by Kartheek Sabbisetty on 25/09/18.
+//  Copyright © 2018 Kartheek Sabbisetty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
